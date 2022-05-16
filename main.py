b86ad04ca7c1579f848f4583bc2ff5d402f74e71
Walid AlDari <walidald@my.yorku.ca>
2022-05-16 17:42:30 +0300

    Added Basic Database Configuration

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+db.create_all()
 
 video_post_args = reqparse.RequestParser()
 video_post_args.add_argument("Name", type=str, help="Video Name", required=True)
@@ -11,12 +16,25 @@ video_post_args.add_argument("Likes", type=int, help="Video Likes", required=Tru
 
 videos = {}
 
+def idCheck(videoID):
+    if videoID not in videos:
+        abort(404, message="VideoID doesn't exist")
+
 class Video(Resource):
     def get(self, videoID):
+        idCheck(videoID)
         return videos[videoID]
+
     def post(self, videoID):
         videoProps = video_post_args.parse_args()
-        return {videoID: videoProps}
+        videos[videoID] = videoProps
+        return videos[videoID], 201
+
+    def delete(self, videoID):
+        idCheck(videoID)
+        del videos[videoID]
+        return '', 204
+
 
 # api.add_resource(HelloWorld, "/helloworld")
 api.add_resource(Video, "/video/<string:videoID>")
--- test.py
+++ test.py
@@ -2,6 +2,20 @@ import requests
 
 BASE="http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "video/AE01", {"Name": "Walid", "Views": 300})
+response = requests.post(BASE + "video/AE01", {"Name": "Walid", "Views": 300, "Likes": 70})
 print(response.json())
-print("------")
+
+input()
+
+response = requests.get(BASE + "video/AE01")
+print(response.json())
+
+input()
+
+response = requests.delete(BASE + "video/AE01")
+print(response)
+
+input()
+
+response = requests.get(BASE + "video/AE01")
+print(response.json())
